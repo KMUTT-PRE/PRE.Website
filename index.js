2919501077ddf83bb6d02e2f7090d65a4fb097bc
Potae014 <Sattha.Tongthavornsuwan@gmail.com>
2025-11-08 16:30:41 +0700

    newHomepage

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 5000,
   touch: false,
 });
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += "responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
