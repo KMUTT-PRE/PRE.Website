0b940b9f4e7538e73b666cca9536810f806e2f27
Potae014 <Sattha.Tongthavornsuwan@gmail.com>
2025-12-01 11:11:54 +0700

    ejs

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("pages/Home");
+});
+
+app.get("/pre", (req, res) => {
+  res.render("pages/PRE");
+});
+
+app.listen(port, () => {
+  console.log(`App listening at port ${port}`);
+});
