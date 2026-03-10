513eb66b4af70116843be66aa3c85d77aad51971
Potae014 <Sattha.Tongthavornsuwan@gmail.com>
2026-03-10 12:36:00 +0700

    TABEE

--- app.js
+++ app.js
@@ -57,6 +57,14 @@ app.get("/news", (req, res) => {
   res.render("pages/news");
 });
 
+app.get("/news/pos20260305-1", (req, res) => {
+  res.render("news/2569/news/pos20260305-1");
+});
+
+app.get("/news/pos20260304-1", (req, res) => {
+  res.render("news/2569/news/pos20260304-1");
+});
+
 app.get("/news/pos20260303-1", (req, res) => {
   res.render("news/2569/news/pos20260303-1");
 });
